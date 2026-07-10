d379235bf69c95b5a1033f97369adb6793da51fd
AK-cxyz <abhigyan.k.09@gmail.com>
2026-07-10 22:54:14 +0530

    Update configs.h

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -1150,13 +1150,12 @@
     
       #define KIT_LED_BUILTIN 13
     #endif
-
     #if defined(MARAUDER_V6) || defined(MARAUDER_V6_1)
       #define CHAN_PER_PAGE 7
 
       #define SCREEN_CHAR_WIDTH 40
-      #define HAS_ILI9341
-      //#define HAS_ST7789
+      //#define HAS_ILI9341
+      #define HAS_ST7789
     
       #define BANNER_TEXT_SIZE 2
 
@@ -1187,17 +1186,13 @@
       #define SCREEN_HEIGHT TFT_WIDTH
       #define HEIGHT_1 TFT_WIDTH
       #define WIDTH_1 TFT_HEIGHT
-      #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6) // number of characters on a single line with normal font
-      #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
-      #define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
-      #define TOP_FIXED_AREA 48 // Number of lines in top fixed area (lines counted from top of screen)
-      #define YMAX 320 // Bottom of screen area
+      #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6)
+      #define TEXT_HEIGHT 16
+      #define BOT_FIXED_AREA 0
+      #define TOP_FIXED_AREA 48
+      #define YMAX 320
       #define minimum(a,b)     (((a) < (b)) ? (a) : (b))
-      //#define MENU_FONT NULL
-      #define MENU_FONT &FreeMono9pt7b // Winner
-      //#define MENU_FONT &FreeMonoBold9pt7b
-      //#define MENU_FONT &FreeSans9pt7b
-      //#define MENU_FONT &FreeSansBold9pt7b
+      #define MENU_FONT &FreeMono9pt7b
       #define BUTTON_SCREEN_LIMIT 12
       #define BUTTON_ARRAY_LEN BUTTON_SCREEN_LIMIT
       #define STATUS_BAR_WIDTH 16
@@ -1208,13 +1203,11 @@
       #define FRAME_W 120
       #define FRAME_H 50
     
-      // Red zone size
       #define REDBUTTON_X FRAME_X
       #define REDBUTTON_Y FRAME_Y
       #define REDBUTTON_W (FRAME_W/2)
       #define REDBUTTON_H FRAME_H
     
-      // Green zone size
       #define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
       #define GREENBUTTON_Y FRAME_Y
       #define GREENBUTTON_W (FRAME_W/2)
